41bda3961125f7e24724a1d2dbcf522b29937d49
dogaanismail <ismaildogaan@gmail.com>
2020-11-11 21:15:36 +0300

    DevPlatform - Authentication register and login components have been added into the system.

--- DevPlatform.Api/DevPlatformSpa/src/app/app-routing.module.ts
+++ DevPlatform.Api/DevPlatformSpa/src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
+import { ProfileLayoutComponent } from './components/profile/profile-layout/profile-layout.component';
+import { QuestionLayoutComponent } from './components/question/question-layout/question-layout.component';
 import { TimelineLayoutComponent } from './components/timeline/timeline-layout/timeline-layout.component';
 
 const routes: Routes = [
   {
     path: '', component: TimelineLayoutComponent, pathMatch: 'full',
     loadChildren: () => import('./components/timeline/timeline.module').then(m => m.TimelineModule)
-  }
+  },
+  {
+    path: "profile", component: ProfileLayoutComponent,  //TODO: must be profile/:username
+    loadChildren: () => import('./components/profile/profile.module').then(m => m.ProfileModule)
+  },
+  {
+    path: "question", component: QuestionLayoutComponent,
+    loadChildren: () => import('./components/question/question.module').then(m => m.QuestionModule)
+  },
+  { path: "account/login", component: LoginComponent },
+  { path: "account/register", component: RegisterComponent }
+
+  //TODO: Import the other modules here
 ]
 
 @NgModule({
--- DevPlatform.Api/DevPlatformSpa/src/app/app.module.ts
+++ DevPlatform.Api/DevPlatformSpa/src/app/app.module.ts
@@ -1,16 +1,25 @@
+
+/* Components */
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
+import { NavbarComponent } from './components/shared/navbar/navbar-layout/navbar.component';
+
+/* Modules */
 import { QuestionModule } from './components/question/question.module';
 import { ProfileModule } from './components/profile/profile.module';
-import { NavbarComponent } from './components/shared/navbar/navbar-layout/navbar.component';
 import { TimelineModule } from './components/timeline/timeline.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
