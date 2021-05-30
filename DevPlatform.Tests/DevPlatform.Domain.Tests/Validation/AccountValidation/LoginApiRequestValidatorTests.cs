601701bf64d106d81c2cd7af2a09787ac09f4015
ismail doğan <ismaildogaan@gmail.com>
2021-05-30 04:30:22 +0300

    Task-33 DevPlatform - Domain layer test scenarios such as validators have been added into the system.

---
+++ DevPlatform.Tests/DevPlatform.Domain.Tests/Validation/AccountValidation/LoginApiRequestValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using DevPlatform.Domain.Api;
+using DevPlatform.Domain.Validation;
+using DevPlatform.Tests.DevPlatform.Services.Tests;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace DevPlatform.Tests.DevPlatform.Domain.Tests.Validation.AccountValidation
+{
+    [TestFixture]
+    public class LoginApiRequestValidatorTests : ServiceTest
+    {
+        private LoginApiRequestValidator _loginApiRequestValidator;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _loginApiRequestValidator = GetService<LoginApiRequestValidator>();
+        }
+
+        [Test]
+        public void ShouldHaveErrorWhenUserNameIsNullOrEmpty()
+        {
+            var model = new LoginApiRequest
+            {
+                UserName = null
+            };
+
+            _loginApiRequestValidator.ShouldHaveValidationErrorFor(x => x.UserName, model);
+
+            model.UserName = string.Empty;
+            _loginApiRequestValidator.ShouldHaveValidationErrorFor(x => x.UserName, model);
+        }
+
+        [Test]
+        public void ShouldHaveErrorWhenPasswordIsNullOrEmpty()
+        {
+            var model = new LoginApiRequest
+            {
+                Password = null
+            };
+
+            _loginApiRequestValidator.ShouldHaveValidationErrorFor(x => x.Password, model);
+
+            model.Password = string.Empty;
+            _loginApiRequestValidator.ShouldHaveValidationErrorFor(x => x.Password, model);
+        }
+    }
+}
---
+++ DevPlatform.Tests/DevPlatform.Domain.Tests/Validation/PortalValidation/PostCreateApiValidatorTests.cs
@@ -0,0 +1,34 @@
+﻿using DevPlatform.Domain.Api;
+using DevPlatform.Domain.Validation;
+using DevPlatform.Tests.DevPlatform.Services.Tests;
+using FluentValidation.TestHelper;
+using NUnit.Framework;
+
+namespace DevPlatform.Tests.DevPlatform.Domain.Tests.Validation.PortalValidation
+{
+    [TestFixture]
+    public class PostCreateApiValidatorTests : ServiceTest
+    {
+        private PostCreateApiValidator _postCreateApiRequestValidator;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _postCreateApiRequestValidator = GetService<PostCreateApiValidator>();
+        }
+
+        [Test]
+        public void ShouldHaveErrorWhenTextIsNullOrEmpty()
+        {
+            var model = new PostCreateApi
+            {
+                Text = null
+            };
+
+            _postCreateApiRequestValidator.ShouldHaveValidationErrorFor(x => x.Text, model);
+
+            model.Text = string.Empty;
+            _postCreateApiRequestValidator.ShouldHaveValidationErrorFor(x => x.Text, model);
+        }
+    }
+}
