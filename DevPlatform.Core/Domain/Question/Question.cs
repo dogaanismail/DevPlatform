04e3079cb862bd9f819675b2e75d66779dfb6ff4
ismail doğan <ismaildogaan@gmail.com>
2021-06-05 01:56:29 +0300

    Task-37 DevPlatform - Question data migration classeses have been added into the system.

---
+++ DevPlatform.Core/Domain/Question/Question.cs
@@ -0,0 +1,15 @@
+﻿using DevPlatform.Core.Entities;
+using LinqToDBAssociation = LinqToDB.Mapping;
+using System.Collections.Generic;
+
+namespace DevPlatform.Core.Domain.Question
+{
+    public partial class Question : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        [LinqToDBAssociation.Association(ThisKey = nameof(Id), OtherKey = nameof(QuestionComment.QuestionId), CanBeNull = true)]
+        public virtual ICollection<QuestionComment> QuestionComments { get; set; }
+    }
+}
---
+++ DevPlatform.Core/Domain/Question/QuestionComment.cs
@@ -0,0 +1,19 @@
+﻿using DevPlatform.Core.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using LinqToDBAssociation = LinqToDB.Mapping;
+
+namespace DevPlatform.Core.Domain.Question
+{
+    public partial class QuestionComment : BaseEntity
+    {
+        public string Text { get; set; }
+
+        [Required]
+        [ForeignKey("CommentQuestion")]
+        public int QuestionId { get; set; }
+
+        [LinqToDBAssociation.Association(ThisKey = nameof(QuestionId), OtherKey = nameof(Question.Id), CanBeNull = true)]
+        public virtual Question CommentQuestion { get; set; }
+    }
+}
